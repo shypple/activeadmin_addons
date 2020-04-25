df3752923d2504ea35ac6d92585769055ed86ab3
Dirk Jonker <dirkjonker@gmail.com>
2020-04-25 13:01:14 +0200

    Don't create a new body element for data attribute

--- lib/activeadmin_addons/active_admin_config.rb
+++ lib/activeadmin_addons/active_admin_config.rb
@@ -3,6 +3,7 @@ class ActiveAdmin::Views::Pages::Base
 
   def build(*args)
     original_build(args)
+    body = find_by_tag("body").first
     body.set_attribute "data-default-select", ActiveadminAddons.default_select
   end
 end
